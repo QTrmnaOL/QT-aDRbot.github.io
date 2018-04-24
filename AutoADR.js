1fdfd8f518bd2bdc65f7195e54084f4bb33168f0
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2018-04-24 12:25:05 +0200

    Edited from AT to ADR

--- AutoADR.js
+++ AutoADR.js
@@ -1,24 +1,91 @@
 // ==UserScript==
-// @name         QT - AutoTrimpsV2+genBTC
-// @namespace    https://github.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL
-// @version      QT - 2.1.5.2-genbtc-12-23-2016+Modular
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
 // @description  try to take over the world!
-// @author       zininzinin, spindrjr, belaith, ishakaru, genBTC
-// @include      *trimps.github.io*
-// @include      *kongregate.com/games/GreenSatellite/trimps
+// @author       You
+// @match        http://adarkroom.doublespeakgames.com/
 // @grant        none
 // ==/UserScript==
-window.alert("script source loaded");
-var ADRVersion = 'QT - 2.1.5.2-genbtc-12-23-2016+Modular';
 
-////////////////////////////////////////////////////////////////////////////////
-//Main Loader Initialize Function (loads first, load everything else)///////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////
-var atscript = document.getElementById('AutoTrimps-script')
-  , base = 'https://raw.githubusercontent.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/master/'
-  , module = 'modules/'
-  ;
-if (atscript !== null) {
-    base = atscript.getAttribute('src').replace(/AutoTrimps2\.js$/, '');
-}
+(function() {
+    'use strict';
+
+
+    if ( window.AG ) {
+        window.AG.reset();
+        delete window.AG;
+    }
+
+    var AutoGame = function() {
+        /* Buttons */
+        this.gather_btn = $('#gatherButton');
+        this.traps_btn = $('#trapsButton');
+        this.buildcart_btn = $('#build_cart');
+        this.buildtrap_btn = $('#build_trap');
+        this.buildhut_btn = $('#build_hut');
+        this.buildlodge_btn = $('#build_lodge');
+        this.stoke_btn = $('#stokeButton');
+        this.notification = $('#notification');
+        this.ruinedtrap_btn =  $('#ignore');
+        this.lonelyhut_btn = $('#location_outside');
+        this.room_btn = $('#location_room');
+
+        /* functions */
+        this.gather_timeout = null;
+        this.room = null;
+        this.village = null;
+        this.build_timeout = null;
+
+        /* Resources */
+        this.wood = null;
+var i;
+    };
+
+    AutoGame.prototype.start = function() {
+        var self = this;
+
+        this.room = window.setInterval(function() {
+            self.room_btn.click();
+            /* if ( self.notfication.has
+        /* stoke fire */
+            /* build lodge */
+            if ( !self.buildlodge_btn.hasClass('disabled') ) self.buildlodge_btn.click();
+            /* build cart */
+            if ( !self.buildcart_btn.hasClass('disabled') ) self.buildcart_btn.click();
+            /* build trap */
+            if ( !self.buildtrap_btn.hasClass('disabled') ) self.buildtrap_btn.click();
+            /* build hut */
+            if ( !self.buildhut_btn.hasClass('disabled') ) self.buildhut_btn.click();
+        },
+                                       5000);
+
+        this.village = window.setInterval(function() {
+            self.lonelyhut_btn.click();
+            /* if ( !self.stoke_btn.hasClass('disabled') ) self.stoke_btn.click();
+		/* Collect wood */
+            if ( !self.gather_btn.hasClass('disabled') ) self.gather_btn.click();
+            /* Collect traps */
+            if ( !self.traps_btn.hasClass('disabled') ) self.traps_btn.click();
+            /* Collect traps */
+            if ( !self.ruinedtrap_btn.hasClass('disabled') ) self.traps_btn.click();
+
+            /* Get Resources */
+            this.wood = document.getElementById('row_wood').children;
+var i;
+/*            for (i = 0; i <= this.wood.length - 1; i++) {
+                console.log(this.wood[i].innerHTML);
+            }*/
+
+           this.wood =  this.wood[1].innerHTML;
+           this.console.log( this.wood );
+        },
+                                          5000);
+
+    };
+
+    window.AG = new AutoGame();
+    AG.start();
+
+
+})();
--- install.user.js
+++ install.user.js
@@ -1,20 +1,14 @@
 // ==UserScript==
-// @name         QT - AutoTrimps-genBTC
-// @namespace    https://github.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/blob/master/
-// @version      QT - 2.1.5.2-genbtc-12-23-2016+Modular
-// @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC
-// @include        *trimps.github.io*
-// @include        *kongregate.com/games/GreenSatellite/trimps
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://adarkroom.doublespeakgames.com/
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-script';
-//script.src = 'https://genbtc.github.io/AutoTrimps-betadev/AutoTrimps2.js';
-script.src = 'https://raw.githubusercontent.com/QTrmnaOL/Autotrimps-v2-alpha-QTrmnaOL/blob/master/AutoTrimps2.js';
+script.id = 'AutoADR-Script';
+script.src = 'https://raw.githubusercontent.com/QTrmnaOL/QTrmnaOL.github.io/master/aDarkRoom/AutoADR.js';
 document.head.appendChild(script);
-
-
-// This File is Deprecated, please use .user.js instead (with the leading dot) so TamperMonkey/Greasemonkey can detect it as a script automatically.
-// Other than that they are identical and no change is required on your part if everything is working already.

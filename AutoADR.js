eec68e0cc491dd3cfc6833657fc0f5da40e144e4
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2018-04-24 15:07:51 +0200

    Earlygame Light + Stoke works

--- AutoADR.js
+++ AutoADR.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         AutoADR
-// @namespace    http://tampermonkey.net/
-// @version      v0.1 alpha
-// @description  try to take over the world!
+// @name         AutoADR Testing
+// @namespace    https://raw.githubusercontent.com/QTrmnaOL/QTrmnaOL.github.io/
+// @version      V 0.1, alpha.
+// @description  try to automate ADR
 // @author       You
 // @match        http://adarkroom.doublespeakgames.com/
 // @grant        none
+// @include 		 *http://adarkroom.doublespeakgames.com/*
 // ==/UserScript==
 
 (function() {
@@ -17,15 +18,19 @@
         delete window.AG;
     }
 
+//GAME Definitions
     var AutoGame = function() {
         /* Buttons */
+        this.stoke_btn = $('#stokeButton');
+ 		this.light_btn = $('#lightButton');
+
         this.gather_btn = $('#gatherButton');
         this.traps_btn = $('#trapsButton');
         this.buildcart_btn = $('#build_cart');
         this.buildtrap_btn = $('#build_trap');
         this.buildhut_btn = $('#build_hut');
         this.buildlodge_btn = $('#build_lodge');
-        this.stoke_btn = $('#stokeButton');
+
         this.notification = $('#notification');
         this.ruinedtrap_btn =  $('#ignore');
         this.lonelyhut_btn = $('#location_outside');
@@ -39,53 +44,81 @@
 
         /* Resources */
         this.wood = null;
-var i;
     };
 
-    AutoGame.prototype.start = function() {
+
+    //GAME START
+AutoGame.prototype.start = function() {
         var self = this;
 
-        this.room = window.setInterval(function() {
-            self.room_btn.click();
-            /* if ( self.notfication.has
-        /* stoke fire */
-            /* build lodge */
-            if ( !self.buildlodge_btn.hasClass('disabled') ) self.buildlodge_btn.click();
-            /* build cart */
-            if ( !self.buildcart_btn.hasClass('disabled') ) self.buildcart_btn.click();
-            /* build trap */
-            if ( !self.buildtrap_btn.hasClass('disabled') ) self.buildtrap_btn.click();
-            /* build hut */
-            if ( !self.buildhut_btn.hasClass('disabled') ) self.buildhut_btn.click();
-        },
-                                       5000);
+//ONCE light up fire.
+if ( !document.getElementById('stokeButton') )
+    {
+ 	self.light_btn.click();
+    console.log( 'Lighted Fire' );
+    }
 
-        this.village = window.setInterval(function() {
-            self.lonelyhut_btn.click();
-            /* if ( !self.stoke_btn.hasClass('disabled') ) self.stoke_btn.click();
-		/* Collect wood */
-            if ( !self.gather_btn.hasClass('disabled') ) self.gather_btn.click();
-            /* Collect traps */
-            if ( !self.traps_btn.hasClass('disabled') ) self.traps_btn.click();
-            /* Collect traps */
-            if ( !self.ruinedtrap_btn.hasClass('disabled') ) self.traps_btn.click();
+//EARLY GAME -> Offload to Function / Module
+this.earlygame_TO = window.setInterval(function() {
+if ( !self.stoke_btn.hasClass('disabled'))
+         {
+ 	     self.stoke_btn.click();
+             console.log( 'Stoked Fire' );
+         }
+   else
+   {
+       console.log( 'Stoke Fire Disabled' );
+   }
+}, 2000);
+//END Earlygame function
 
-            /* Get Resources */
-            this.wood = document.getElementById('row_wood').children;
-var i;
-/*            for (i = 0; i <= this.wood.length - 1; i++) {
-                console.log(this.wood[i].innerHTML);
-            }*/
+}; //End Function Autogame
 
-           this.wood =  this.wood[1].innerHTML;
-           this.console.log( this.wood );
-        },
-                                          5000);
+AutoGame.prototype.reset = function() {
+	if ( this.earlygame_TO )
+		window.clearInterval(this.gather_timeout);
+	if ( this.build_timeout )
+		window.clearInterval(this.build_timeout);
+};
 
-    };
 
+console.log( 'restart');
     window.AG = new AutoGame();
     AG.start();
-
-
 })();
+
+
+
+
+  //   this.room = window.setInterval(function() {
+  //           self.room_btn.click();
+		// 	if ( !self.stoke_btn.hasClass('disabled') ) self.this.stoke_btn.click();
+  //           /* build lodge */
+  //           if ( !self.buildlodge_btn.hasClass('disabled') ) self.buildlodge_btn.click();
+  //           /* build cart */
+  //           if ( !self.buildcart_btn.hasClass('disabled') ) self.buildcart_btn.click();
+  //           /* build trap */
+  //           if ( !self.buildtrap_btn.hasClass('disabled') ) self.buildtrap_btn.click();
+  //           /* build hut */
+  //           if ( !self.buildhut_btn.hasClass('disabled') ) self.buildhut_btn.click();
+  //       },
+  //                                      5000);
+
+  //       this.village = window.setInterval(function() {
+  //           self.lonelyhut_btn.click();
+  //           /* if ( !self.stoke_btn.hasClass('disabled') ) self.stoke_btn.click();
+		// /* Collect wood */
+  //           if ( !self.gather_btn.hasClass('disabled') ) self.gather_btn.click();
+  //           /* Collect traps */
+  //           if ( !self.traps_btn.hasClass('disabled') ) self.traps_btn.click();
+  //           /* Collect traps */
+  //           if ( !self.ruinedtrap_btn.hasClass('disabled') ) self.traps_btn.click();
+  //           /* Get Resources */
+  //           this.wood = document.getElementById('row_wood').children;
+		// 	/* for (i = 0; i <= this.wood.length - 1; i++) {
+  //               console.log(this.wood[i].innerHTML);
+  //           }*/
+  //          this.wood =  this.wood[1].innerHTML;
+  //          this.console.log( this.wood );
+  //       },
+  //                                         5000);

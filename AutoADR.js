982d3fec345c438bada7b1ed4c745c8300c7e9aa
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2018-04-24 16:09:54 +0200

    Reworked Stoke Fire
    Working on Forest now

--- AutoADR.js
+++ AutoADR.js
@@ -44,6 +44,9 @@
 
         /* Resources */
         this.wood = null;
+
+        /* General */
+        this.notification = null;
     };
 
 
@@ -51,73 +54,42 @@
 AutoGame.prototype.start = function() {
         var self = this;
 
-//EARLY GAME -> Offload to Function / Module
-if ( self.stoke_btn.length > 0 )
-    {
- 	self.light_btn.click();
-    } else {
-         if ( self.gather_btn.length > 0 )
-         {
- 	     self.this.stoke_btn.click();
-         }
-    }
-
-    this.room = window.setInterval(function() {
-            self.room_btn.click();
-			if ( !self.stoke_btn.hasClass('disabled') ) self.this.stoke_btn.click();
-            /* build lodge */
-            if ( !self.buildlodge_btn.hasClass('disabled') ) self.buildlodge_btn.click();
-            /* build cart */
-            if ( !self.buildcart_btn.hasClass('disabled') ) self.buildcart_btn.click();
-            /* build trap */
-            if ( !self.buildtrap_btn.hasClass('disabled') ) self.buildtrap_btn.click();
-            /* build hut */
-            if ( !self.buildhut_btn.hasClass('disabled') ) self.buildhut_btn.click();
-        },
-                                       5000);
-
-        this.village = window.setInterval(function() {
-            self.lonelyhut_btn.click();
-            /* if ( !self.stoke_btn.hasClass('disabled') ) self.stoke_btn.click();
-		/* Collect wood */
-            if ( !self.gather_btn.hasClass('disabled') ) self.gather_btn.click();
-            /* Collect traps */
-            if ( !self.traps_btn.hasClass('disabled') ) self.traps_btn.click();
-            /* Collect traps */
-            if ( !self.ruinedtrap_btn.hasClass('disabled') ) self.traps_btn.click();
-            /* Get Resources */
-            this.wood = document.getElementById('row_wood').children;
-			/* for (i = 0; i <= this.wood.length - 1; i++) {
-                console.log(this.wood[i].innerHTML);
-            }*/
-           this.wood =  this.wood[1].innerHTML;
-           this.console.log( this.wood );
-        },
-                                          5000);
-
-
-}; //End Function Autogame
+var stokeCount = 1;
 
 //ONCE light up fire.
-if ( !document.getElementById('stokeButton') )
+if ( document.getElementById('stokeButton') )
     {
  	self.light_btn.click();
     console.log( 'Lighted Fire' );
     }
 
-//EARLY GAME -> Offload to Function / Module
-this.earlygame_TO = window.setInterval(function() {
-if ( !self.stoke_btn.hasClass('disabled'))
-         {
- 	     self.stoke_btn.click();
-             console.log( 'Stoked Fire' );
+//EARLY GAME (stoke) -> Offload to Function / Module
+	this.earlygame_TO = window.setInterval(function() {
+
+    this.notification = document.getElementById('notifications').children;
+        var i = null;
+         for (i = 0; i <= this.notification.length - (this.notification.length + 3); i++) {
+                 console.log(this.notification[i].innerHTML);
+             if ( this.notification[i] == 'the room is cold.' ){
+                  self.stoke_btn.click();
+	             console.log( 'Stoked Fire' );
+            }
          }
-   else
-   {
-       console.log( 'Stoke Fire Disabled' );
-   }
-}, 2000);
-//END Earlygame function
+
+	}, 30000);
+
+//Village
+
+//else{
+	//window.setInterval(function() {
+		//self.stoke_btn.click();
+	      //     console.log( 'Stoked Fire longterm' );
+	        // stokeCount = stokeCount + 1;
+	//}, 30000);
+//}
+
+
+//END Earlygame (stoke) function
 
 }; //End Function Autogame
 
@@ -133,6 +105,7 @@ console.log( 'restart');
     window.AG = new AutoGame();
     AG.start();
 })();
+
 
 
 
@@ -168,4 +141,3 @@ console.log( 'restart');
   //          this.console.log( this.wood );
   //       },
   //                                         5000);
-

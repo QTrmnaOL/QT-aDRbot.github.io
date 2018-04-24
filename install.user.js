cc5ad4650609bf0c07b57aa7ddd34ae2a20d5008
QTrmnaOL <30366008+QTrmnaOL@users.noreply.github.com>
2018-04-24 12:38:03 +0200

    trying to fix script acceptance

--- install.user.js
+++ install.user.js
@@ -1,14 +1,24 @@
 // ==UserScript==
 // @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @namespace    https://raw.githubusercontent.com/QTrmnaOL/QTrmnaOL.github.io/
 // @version      0.1
-// @description  try to take over the world!
+// @description  try to automate ADR
 // @author       You
 // @match        http://adarkroom.doublespeakgames.com/
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
+var testing = 'X'; // X = Testing on
+
 script.id = 'AutoADR-Script';
-script.src = 'https://raw.githubusercontent.com/QTrmnaOL/QT-aDRbot.github.io/master/AutoADR.js';
+
+if ( testing ==  'X')
+	/* Testing Branch */
+	script.src = 'https://raw.githubusercontent.com/QTrmnaOL/QT-aDRbot.github.io/Testing/AutoADR.js';
+
+if ( testing !=  'X')
+	/* Live Branch */
+	script.src = 'https://raw.githubusercontent.com/QTrmnaOL/QTrmnaOL.github.io/master/Autotrimps/AutoTrimps2.js';
+
 document.head.appendChild(script);
